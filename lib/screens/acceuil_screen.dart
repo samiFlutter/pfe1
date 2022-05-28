6085c51497d12706817902c9584a6425f29d82bd
sami <sami.workmail.flutter@gmail.com>
2022-05-28 19:23:48 +0100

    first time

---
+++ lib/screens/acceuil_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class AcceuilScreen extends StatelessWidget {
+  static const String screenRoute = 'acceuil_screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue[100],
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              children: [
+                Container(
+                  height: 400, //4e dirtou ane bach ydgade 4e m3a telephone
+                  padding: const EdgeInsets.all(20),
+                  child: Lottie.asset('images/vote-blue.json'),
+                ),
+                SizedBox(height: 20),
+                FlatButton(
+                  color: Colors.blue,
+                  child: Text('Inscrir'),
+                  onPressed: () {
+                    // Navigator.push(
+                    //     context,
+                    //     MaterialPageRoute(
+                    //         builder: (context) => InscrirScreen()));
+                  },
+                ),
+                FlatButton(
+                  color: Colors.blue,
+                  child: Text('Voter'),
+                  onPressed: () {
+                    // Navigator.push(context,
+                    //     MaterialPageRoute(builder: (context) => Pagenni()));
+                  },
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
